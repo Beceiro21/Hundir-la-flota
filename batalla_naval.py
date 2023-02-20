ed3aea6385436903281f27a020e5766c7d91e98d
Beceiro21 <107202788+Beceiro21@users.noreply.github.com>
2023-02-20 22:37:47 +0100

    segundo commit

---
+++ batalla_naval.py
@@ -0,0 +1,39 @@
+from hundir import func_random
+from hundir import jugador1
+from hundir import jugador2
+from hundir import coloca_jugador1
+from hundir import coloca_maquina
+
+##'¡El juego empieza aqui!'!##
+print("¡Que empiece el juego!")
+print()
+import time
+turno = 1
+
+func_random(input("Nombre 1:"),"Maquina")
+print()
+print("Coloca jugador 1""\n")
+coloca_jugador1()
+print()
+print("Coloca jugador 2""\n")
+coloca_maquina()
+
+
+while "#" in jugador1.tablero1.values or "#" in jugador2.tablero2.values:
+
+    print("Turno:", turno)
+    time.sleep(1)
+    print("Ataca el jugador 1")
+    jugador1.atacar(jugador2)
+    print("Ataca el jugador 2")
+    jugador2.atacar(jugador1)
+    turno = turno + 1
+    print("---------")
+
+else:
+    if "#" in jugador1.tablero1.values:
+        print("¡Gana el jugador 1!")
+    elif "#" in jugador2.tablero2.values:
+        print("¡Gana el jugador 2!")
+    else:
+        "GAME OVER, caputto"
